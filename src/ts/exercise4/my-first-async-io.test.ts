233e779e29eafb406418d512e22c2b5cd7e01d90
Carles <cpcastells@gmail.com>
2023-09-25 12:13:57 +0200

    Add custom exercise 4 with test

--- src/ts/exercise4/my-first-async-io.test.ts
+++ src/ts/exercise4/my-first-async-io.test.ts
@@ -1,11 +1,12 @@
 import { execSync } from "child_process";
+import fs from "fs";
 
-describe("Given a my-first-io script", () => {
+describe("Given a my-first-async-io script", () => {
   describe("When it is executed with a path of a text file which contains 3 lines", () => {
     test("Then it should log the number of lines in that file", () => {
       const expectedLines = "3";
       const command =
-        "npx ts-node ./src/ts/exercise3/my-first-io.ts ./src/ts/exercise3/test.txt";
+        "npx ts-node ./src/ts/exercise4/my-first-async-io.ts ./src/ts/exercise4/test.txt";
 
       const output = execSync(command).toString().trim();
 
