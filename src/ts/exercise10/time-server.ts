fbfeafc4890a7efab95e9cb5a216e0abe99b9d5a
Carles Pueyo <cpcastells@gmail.com>
2023-10-03 20:49:40 +0200

    Add custom exercise 10

---
+++ src/ts/exercise10/time-server.ts
@@ -0,0 +1,27 @@
+import net from "net";
+
+const port = process.argv[2];
+const date = new Date();
+
+const fillWithZero = (data: number) => (data < 10 ? "0" : "") + data;
+
+const server = net.createServer((socket) => {
+  socket.write(
+    date.getFullYear() +
+      "-" +
+      fillWithZero(date.getMonth() + 1) +
+      "-" +
+      fillWithZero(date.getDate()) +
+      " " +
+      fillWithZero(date.getHours()) +
+      ":" +
+      fillWithZero(date.getMinutes()) +
+      "\n",
+  );
+
+  socket.end();
+});
+
+server.listen(port, () => {
+  console.log(`Listening to the port: ${port}`);
+});
