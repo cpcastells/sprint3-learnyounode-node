3cf3e6eafbf972b68fa6a1f3ff0f390296acfaf2
Carles Pueyo <cpcastells@gmail.com>
2023-10-03 12:47:30 +0200

    Add custom exercise 7 with test

---
+++ src/ts/exercise7/http-client.ts
@@ -0,0 +1,17 @@
+import http from "http";
+
+const url = process.argv[2];
+
+http.get(url, (res) => {
+  res.setEncoding("utf-8");
+
+  res.on("error", (error) => {
+    console.log(error);
+  });
+
+  console.log("Content: ");
+
+  res.on("data", (data) => {
+    console.log(data);
+  });
+});
